d78266ebb5410b545682853df0d9985ae452bfc9
Antonio Aguilera <antonio.aguilerimon@gmail.com>
2021-09-03 14:27:06 -0500

    Cambios en las llaves de BD y correccion en la relacion ruta/controlador en data

--- server/build/keys.js
+++ server/build/keys.js
@@ -4,7 +4,7 @@ exports.default = {
     database: {
         host: 'localhost',
         user: 'root',
-        password: 'whateverpass',
-        database: 'ventas'
+        password: '',
+        database: 'db_ventas'
     }
 };
