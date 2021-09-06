28daa4e2712d35831ac6a9822956c3e48c1da6d6
PabloPerezMedel <pabloperezmedel@hotmail.com>
2021-09-06 12:21:33 -0500

    Cambios

--- server/build/keys.js
+++ server/build/keys.js
@@ -4,7 +4,7 @@ exports.default = {
     database: {
         host: 'localhost',
         user: 'root',
-        password: 'Admin',
+        password: 'Admin1',
         database: 'db_ventas'
     }
 };
--- server/src/keys.ts
+++ server/src/keys.ts
@@ -3,7 +3,7 @@ export default {
     database:{
         host:'localhost',
         user:'root',
-        password:'Admin',
+        password:'Admin1',
         database:'db_ventas'
     }
 }
