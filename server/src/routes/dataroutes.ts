e70456e66382d5f8e7e1ffb1b6418c5f0e8ac451
Ricardoddm <62355992+Ricardoddm@users.noreply.github.com>
2021-09-08 12:42:37 -0500

    rutas para cambios

--- server/src/routes/dataroutes.ts
+++ server/src/routes/dataroutes.ts
@@ -1,4 +1,3 @@
-
 import {Router} from 'express';
 import dataController from '../controllers/datacontrollers';
 class DataRoutes{
@@ -6,8 +5,13 @@ class DataRoutes{
     constructor(){
         this.config();
     }
-   config(): void{
-        this.router.get('/', dataController.index);
+   config(): void{//metodos para que la pagina reaccione
+    //cada metodo se manda a llamar del archivo dataroutes.ts
+        this.router.get('/', dataController.list);//anteriormente llamado index y nos da todos los juegoss
+        this.router.get('/:id', dataController.getOne);//para obtener solo un dato
+        this.router.post('/', dataController.create);
+        this.router.delete('/:id', dataController.delete);
+        this.router.put('/:id', dataController.update);
     }
 }
 const dataRoutes =new DataRoutes();
