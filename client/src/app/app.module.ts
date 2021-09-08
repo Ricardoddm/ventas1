d4cda25620ae15811268ff96f33e1031ca37179e
Antonio Aguilera <antonio.aguilerimon@gmail.com>
2021-09-08 12:42:55 -0500

    Barra de navegacion Angular

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,17 +1,23 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent
   ],
+
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
+  
   providers: [],
   bootstrap: [AppComponent]
 })
