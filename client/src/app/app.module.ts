05202daabb8c9aa7facc93a9186c63acaf4d8851
Fragoza12 <90058444+Fragoza12@users.noreply.github.com>
2021-09-09 13:01:58 -0500

    Cambios

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,17 +1,27 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SaleListComponent } from './sale-list/sale-list.component';
+import { NavegationComponent } from './navigation/navegation.component';
+import { RegisterSaleComponent } from './register-sale/register-sale.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SaleListComponent,
+    NavegationComponent,
+    RegisterSaleComponent
   ],
+
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
+  
   providers: [],
   bootstrap: [AppComponent]
 })
