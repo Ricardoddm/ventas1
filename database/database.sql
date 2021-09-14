bdf948e5651893bf103e40d9a5d3c8f082279620
PabloPerezMedel <pabloperezmedel@hotmail.com>
2021-09-14 14:40:43 -0500

    Corrección de la BD

--- database/database.sql
+++ database/database.sql
@@ -1,51 +1,76 @@
-CREATE TABLE contacto (
+CREATE DATABASE db_ventas;
+
+USE db_ventas;
+
+CREATE TABLE contactos(
     id_contacto INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
     nombre_contacto VARCHAR(50) NOT NULL,
-    telefono VARCHAR(10),
-    email VARCHAR(50),
+    telefono VARCHAR(12) NOT NULL,
+    email VARCHAR(50)NOT NULL,
     rfc VARCHAR(13) NOT NULL
 );
 
-CREATE TABLE empresa (
-    id_empresa INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    nombre_empresa VARCHAR(20) NOT NULL,
+CREATE TABLE lugares(
+	id_lugar INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    pais VARCHAR(50) NOT NULL,
+    estado VARCHAR(50) NOT NULL,
+    direccion VARCHAR(70)NOT NULL,
+    cp INT(5) NOT NULL
+);
+
+CREATE TABLE compradores(
+	id_comprador INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    empresa_comprador VARCHAR(70) NOT NULL,
     id_contacto INT(10),
-    direccion VARCHAR(30) NOT NULL,
-    rfc VARCHAR(12) NOT NULL,
-    CONSTRAINT fk_contacto FOREIGN KEY(id_contacto) REFERENCES contacto(id_contacto)
+    id_lugar INT(10),
+    rfc VARCHAR(13) NOT NULL,
+    CONSTRAINT fk_contacto FOREIGN KEY(id_contacto) REFERENCES contactos(id_contacto),
+    CONSTRAINT fk_lugar FOREIGN KEY(id_lugar) REFERENCES lugares(id_lugar)  
 );
 
-CREATE TABLE vendedor (
+CREATE TABLE vendedores (
     id_vendedor INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    nombre_vendedor VARCHAR(20) NOT NULL,
-    telefono VARCHAR(10) NOT NULL,
-    email VARCHAR(20) NOT NULL
+    nombre_vendedor VARCHAR(70) NOT NULL,
+    telefono VARCHAR(12) NOT NULL,
+    email VARCHAR(40) NOT NULL
 );
 
-CREATE TABLE producto (
+CREATE TABLE productos(
     id_producto INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    nombre_producto VARCHAR(30) NOT NULL,
+    nombre_producto VARCHAR(40) NOT NULL,
     descripcion TEXT NOT NULL,
     precio_unitario DECIMAL(7,2) NOT NULL,
-    existencia INT(7) NOT NULL
+    existencia INT(7) NOT NULL,
+    garantia INT(7) NOT NULL
 );
 
-CREATE TABLE ventas (
-    id_venta INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE ventas(
+	id_venta INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
     fecha timestamp NOT NULL DEFAULT current_timestamp,
-    id_empresa INT(10),
+    id_comprador INT(10),
     id_vendedor INT(10),
     id_producto INT(10),
-    subtotal DECIMAL(7,2),
-    cantidad INT(5),
-    total DECIMAL(7,2),
-    CONSTRAINT fk_empresa FOREIGN KEY(id_empresa) REFERENCES empresa(id_empresa),
-    CONSTRAINT fk_vendedor FOREIGN KEY(id_vendedor) REFERENCES vendedor(id_vendedor), 
-    CONSTRAINT fk_producto FOREIGN KEY(id_producto) REFERENCES producto(id_producto)
+    cantidad INT(5) NOT NULL,
+    subtotal DECIMAL(7,2) NOT NULL,
+    IVA DECIMAL(7,2) NOT NULL,
+    total DECIMAL(7,2)NOT NULL,
+    CONSTRAINT fk_comprador FOREIGN KEY(id_comprador) REFERENCES compradores(id_comprador),
+    CONSTRAINT fk_vendedor FOREIGN KEY(id_vendedor) REFERENCES vendedores(id_vendedor), 
+    CONSTRAINT fk_producto FOREIGN KEY(id_producto) REFERENCES productos(id_producto)
 );
 
-INSERT INTO contacto (nombre_contacto, telefono, email, rfc) 
+CREATE TABLE facturas(
+  id_factura INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  id_venta INT(10),
+  CONSTRAINT fk_venta FOREIGN KEY(id_venta) REFERENCES ventas(id_venta)
+);
+
+
+/*INSERT INTO contactos (nombre_contacto, telefono, email, rfc) 
 VALUES ('Antonio Aguilera', '4492918634', 'anto@gmail.com', 'AUMA310898FRT');
 
-INSERT INTO empresa (nombre_empresa, id_contacto, direccion, rfc)
-VALUES ('Investel', '2', 'Av. Aguascalientes #234', 'AFTYG987');
+INSERT INTO lugares (id_lugar, pais, estado, direccion, cp) 
+VALUES (2, 'México', 'Aguascalientes', 'Av. Arqueros 455', '20660');
+
+INSERT INTO compradores (id_comprador, empresa_comprador, id_contacto, id_lugar, rfc)
+VALUES (1,'Investel', '2', '1', 'AFTYG987');*/
--- database/db_ventas.sql
+++ database/db_ventas.sql
@@ -24,85 +24,105 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `contacto`
+-- Estructura de tabla para la tabla `contactos`
 --
 
-CREATE TABLE `contacto` (
-  `id_contacto` int(10) NOT NULL,
-  `nombre_contacto` varchar(50) NOT NULL,
-  `telefono` varchar(10) DEFAULT NULL,
-  `email` varchar(20) DEFAULT NULL,
-  `rfc` varchar(13) NOT NULL
+CREATE TABLE 'contactos' (
+  'id_contacto' INT(10) NOT NULL,
+  'nombre_contacto' VARCHAR(50) NOT NULL,
+  'telefono' VARCHAR(12) DEFAULT NULL,
+  'email' VARCHAR(50) DEFAULT NULL,
+  'rfc' VARCHAR(13) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Volcado de datos para la tabla `contacto`
+-- Volcado de datos para la tabla `contactos`
 --
 
-INSERT INTO `contacto` (`id_contacto`, `nombre_contacto`, `telefono`, `email`, `rfc`) VALUES
+INSERT INTO 'contactos' ('id_contacto', 'nombre_contacto', 'telefono', 'email', 'rfc') VALUES
 (1, 'Juanito Perez', '4492918634', 'antonio.aguilerimon@', 'AUMA31089845');
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `empresa`
+-- Estructura de tabla para la tabla `lugares`
 --
 
-CREATE TABLE `empresa` (
-  `id_empresa` int(10) NOT NULL,
-  `nombre_empresa` varchar(20) NOT NULL,
-  `id_contacto` int(10) DEFAULT NULL,
-  `direccion` varchar(30) NOT NULL,
-  `rfc` varchar(12) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Volcado de datos para la tabla `empresa`
---
-
-INSERT INTO `empresa` (`id_empresa`, `nombre_empresa`, `id_contacto`, `direccion`, `rfc`) VALUES
-(1, 'Investel', 1, 'Av. Aguascalientes Sur', 'INVT78411');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `producto`
---
-
-CREATE TABLE `producto` (
-  `id_producto` int(10) NOT NULL,
-  `nombre_producto` varchar(30) NOT NULL,
-  `descripcion` text NOT NULL,
-  `precio_unitario` decimal(7,2) NOT NULL,
-  `existencia` int(7) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Volcado de datos para la tabla `producto`
---
-
-INSERT INTO `producto` (`id_producto`, `nombre_producto`, `descripcion`, `precio_unitario`, `existencia`) VALUES
-(1, 'Silla ejecutiva', 'Silla ergonómica para sentarse :p', '1200.00', 50);
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `vendedor`
---
-
-CREATE TABLE `vendedor` (
-  `id_vendedor` int(10) NOT NULL,
-  `nombre_vendedor` varchar(20) NOT NULL,
-  `telefono` varchar(10) NOT NULL,
-  `email` varchar(20) NOT NULL
+CREATE TABLE `lugares` (
+  `id_lugar` INT(10) NOT NULL,
+  `pais` VARCHAR(50) NOT NULL,
+  `estado` VARCHAR(50) NOT NULL,
+  `direccion` VARCHAR(70) NOT NULL,
+  `cp` INT(5) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `vendedor`
 --
 
-INSERT INTO `vendedor` (`id_vendedor`, `nombre_vendedor`, `telefono`, `email`) VALUES
+INSERT INTO `lugares` (`id_lugar`, `pais`, `estado`, `direccion`, `cp`) VALUES
+(1, 'México', 'Aguascalientes', 'Av. Arqueros 455', '20266');
+
+-- --------------------------------------------------------
+--
+-- Estructura de tabla para la tabla `compradores`
+--
+
+CREATE TABLE `compradores` (
+	  'id_comprador' INT(10) NOT NULL,
+    'empresa_comprador' VARCHAR(70) NOT NULL,
+    'id_contacto' INT(10) DEFAULT NULL,
+    'id_lugar' INT(10) NOT NULL,
+    'rfc' VARCHAR(13) NOT NULL,
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `compradores`
+--
+
+INSERT INTO `compradores` (`id_comprador`, `empresa_comprador`, `id_contacto`, `id_lugar`, `rfc`) VALUES
+(1, 'Investel', 1, 1, 'INVT78411');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `vendedores`
+--
+
+CREATE TABLE `vendedores` (
+  `id_vendedor` int(10) NOT NULL,
+  `nombre_vendedor` varchar(70) NOT NULL,
+  `telefono` varchar(12) NOT NULL,
+  `email` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `vendedor`
+--
+
+INSERT INTO `vendedores` (`id_vendedor`, `nombre_vendedor`, `telefono`, `email`) VALUES
 (1, 'Antonio Martinez', '4494064398', 'a.gmail.com');
+
+-- --------------------------------------------------------
+--
+-- Estructura de tabla para la tabla `productos
+--
+
+CREATE TABLE `productos` (
+  `id_producto` int(10) NOT NULL,
+  `nombre_producto` varchar(40) NOT NULL,
+  `descripcion` text NOT NULL,
+  `precio_unitario` decimal(7,2) NOT NULL,
+  `existencia` int(7) NOT NULL,
+  'garantia' INT(7) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `productos`
+--
+
+INSERT INTO `productos` (`id_producto`, `nombre_producto`, `descripcion`, `precio_unitario`, `existencia`, `garantia`) VALUES
+(1, 'Silla ejecutiva', 'Silla ergonómica para sentarse :p', '1200.00', 50, 6);
 
 -- --------------------------------------------------------
 
@@ -113,7 +133,7 @@ INSERT INTO `vendedor` (`id_vendedor`, `nombre_vendedor`, `telefono`, `email`) V
 CREATE TABLE `ventas` (
   `id_venta` int(10) NOT NULL,
   `fecha` timestamp NOT NULL DEFAULT current_timestamp(),
-  `id_empresa` int(10) DEFAULT NULL,
+  `id_comprador` int(10) DEFAULT NULL,
   `id_vendedor` int(10) DEFAULT NULL,
   `id_producto` int(10) DEFAULT NULL,
   `subtotal` decimal(7,2) DEFAULT NULL,
@@ -125,75 +145,115 @@ CREATE TABLE `ventas` (
 -- Volcado de datos para la tabla `ventas`
 --
 
-INSERT INTO `ventas` (`id_venta`, `fecha`, `id_empresa`, `id_vendedor`, `id_producto`, `subtotal`, `cantidad`, `total`) VALUES
+INSERT INTO `ventas` (`id_venta`, `fecha`, `id_comprador`, `id_vendedor`, `id_producto`, `subtotal`, `cantidad`, `total`) VALUES
 (1, '2021-09-09 22:47:50', 1, 1, 1, '3500.00', 3500, '3500.00'),
 (2, '2021-09-09 22:56:42', 1, 1, 1, '4000.00', 4000, '4000.00');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `facturas`
+--
+
+CREATE TABLE `facturas` (
+  `id_factura` int(10) NOT NULL,
+  `id_venta` int(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `facturas`
+--
+
+INSERT INTO `facturas` (`id_factura`, `id_venta`) VALUES
+(1, 1);
+
+
 
 --
 -- Índices para tablas volcadas
 --
 
 --
--- Indices de la tabla `contacto`
+-- Indices de la tabla `contactos`
 --
-ALTER TABLE `contacto`
+ALTER TABLE `contactos`
   ADD PRIMARY KEY (`id_contacto`);
 
 --
--- Indices de la tabla `empresa`
+-- Indices de la tabla `lugares`
 --
-ALTER TABLE `empresa`
-  ADD PRIMARY KEY (`id_empresa`),
+ALTER TABLE `lugares`
+  ADD PRIMARY KEY (`id_lugares`);
+
+--
+-- Indices de la tabla `compradores`
+--
+ALTER TABLE `compradores`
+  ADD PRIMARY KEY (`id_comprador`),
   ADD KEY `fk_contacto` (`id_contacto`);
 
 --
--- Indices de la tabla `producto`
+-- Indices de la tabla `vendedores`
 --
-ALTER TABLE `producto`
-  ADD PRIMARY KEY (`id_producto`);
+ALTER TABLE `vendedores`
+  ADD PRIMARY KEY (`id_vendedor`);
 
 --
--- Indices de la tabla `vendedor`
+-- Indices de la tabla `productos`
 --
-ALTER TABLE `vendedor`
-  ADD PRIMARY KEY (`id_vendedor`);
+ALTER TABLE `productos`
+  ADD PRIMARY KEY (`id_producto`);
 
 --
 -- Indices de la tabla `ventas`
 --
 ALTER TABLE `ventas`
   ADD PRIMARY KEY (`id_venta`),
-  ADD KEY `fk_empresa` (`id_empresa`),
+  ADD KEY `fk_comrpador` (`id_comprador`),
   ADD KEY `fk_vendedor` (`id_vendedor`),
   ADD KEY `fk_producto` (`id_producto`);
+
+--
+-- Indices de la tabla `facturas`
+--
+ALTER TABLE `factuas`
+  ADD PRIMARY KEY (`id_factura`),
+  ADD KEY `fk_venta` (`id_venta`)
+
+
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
 --
--- AUTO_INCREMENT de la tabla `contacto`
+-- AUTO_INCREMENT de la tabla `contactos`
 --
-ALTER TABLE `contacto`
+ALTER TABLE `contactos`
   MODIFY `id_contacto` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT de la tabla `empresa`
---
-ALTER TABLE `empresa`
-  MODIFY `id_empresa` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+-- AUTO_INCREMENT de la tabla `lugares`
+ALTER TABLE `lugares`
+  MODIFY `id_lugar` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT de la tabla `producto`
+-- AUTO_INCREMENT de la tabla `compradores`
 --
-ALTER TABLE `producto`
-  MODIFY `id_producto` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+ALTER TABLE `compradores`
+  MODIFY `id_comprador` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT de la tabla `vendedor`
+-- AUTO_INCREMENT de la tabla `vendedores`
 --
-ALTER TABLE `vendedor`
+ALTER TABLE `vendedores`
   MODIFY `id_vendedor` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `productos`
+--
+ALTER TABLE `productos`
+  MODIFY `id_producto` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT de la tabla `ventas`
@@ -202,22 +262,38 @@ ALTER TABLE `ventas`
   MODIFY `id_venta` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
+-- AUTO_INCREMENT de la tabla `facturas`
+--
+ALTER TABLE `facturas`
+  MODIFY `id_factura` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+
+
+--
 -- Restricciones para tablas volcadas
 --
 
 --
--- Filtros para la tabla `empresa`
+-- Filtros para la tabla `compradores`
 --
-ALTER TABLE `empresa`
-  ADD CONSTRAINT `fk_contacto` FOREIGN KEY (`id_contacto`) REFERENCES `contacto` (`id_contacto`);
+ALTER TABLE `compradores`
+  ADD CONSTRAINT `fk_contacto` FOREIGN KEY (`id_contacto`) REFERENCES `contactos` (`id_contacto`);
+  ADD CONSTRAINT `fk_lugar` FOREIGN KEY (`id_lugar`) REFERENCES `lugares` (`id_lugar`);
 
 --
 -- Filtros para la tabla `ventas`
 --
 ALTER TABLE `ventas`
-  ADD CONSTRAINT `fk_empresa` FOREIGN KEY (`id_empresa`) REFERENCES `empresa` (`id_empresa`),
-  ADD CONSTRAINT `fk_producto` FOREIGN KEY (`id_producto`) REFERENCES `producto` (`id_producto`),
-  ADD CONSTRAINT `fk_vendedor` FOREIGN KEY (`id_vendedor`) REFERENCES `vendedor` (`id_vendedor`);
+  ADD CONSTRAINT `fk_comprador` FOREIGN KEY (`id_comprador`) REFERENCES `compradores` (`id_comprador`),
+  ADD CONSTRAINT `fk_producto` FOREIGN KEY (`id_producto`) REFERENCES `productos` (`id_producto`),
+  ADD CONSTRAINT `fk_vendedor` FOREIGN KEY (`id_vendedor`) REFERENCES `vendedores` (`id_vendedor`);
+
+--
+-- Filtros para la tabla `facturas`
+--
+ALTER TABLE `facturas`
+  ADD CONSTRAINT `fk_venta` FOREIGN KEY (`id_venta`) REFERENCES `ventas` (`id_venta`);
+
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
