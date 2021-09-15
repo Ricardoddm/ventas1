052b5245aca9ee586f0a315ac154051c4be92498
Antonio Aguilera <antonio.aguilerimon@gmail.com>
2021-09-14 19:14:56 -0500

    NodeJS: Se elimino la carpeta build

--- server/build/controllers/indexcontroller.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.indexController = void 0;
-class IndexController {
-    index(req, res) {
-        res.send('Index'); //se puede hacer lo mismo con el objeto JSON  res.json({text:'texto'});
-    }
-}
-exports.indexController = new IndexController();
