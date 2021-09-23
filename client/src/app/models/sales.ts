218f6b80df2d1868f61b7bcb53a1c86d1a1c40c8
ricardoddm <ricky.dd@hotmail.com>
2021-09-23 12:19:42 -0500

    23 sep 1

--- client/src/app/models/sales.ts
+++ client/src/app/models/sales.ts
@@ -1,8 +1,19 @@
-export interface Sale{
 
-    id_contacto?: number|any,
+import { Title } from "@angular/platform-browser";
+
+export interface Sale{
+    /*id_contacto?: number,
     nombre_contacto?: string,
     telefono?: string,
-    correo?: string,
-    RFC?: string
+    email?: string,
+    rfc?: string*/
+
+    id_venta?: number,
+    fecha?: Date,
+    id_empresa?: number,
+    id_vendedor?: number,
+    id_producto?: number,
+    subtotal?: number,
+    cantidad?: number,
+    total?: number   
 }
